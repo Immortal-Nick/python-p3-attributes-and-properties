ae66a6c3f95152aa587ae813513af2ff44484232
Nixon <kiplangatnixon28@gmail.com>
2024-09-06 15:20:41 +0300

    creates property function

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,31 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name: str = "", job: str = ""):
+        # Initialize properties using setters to ensure validation
+        self._name = None
+        self._job = None
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value: str):
+        if not isinstance(value, str) or not (1 <= len(value) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            self._name = value.title()
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value: str):
+        if value not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            self._job = value
